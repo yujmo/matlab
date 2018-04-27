3f04a4ac7981f09a3cb992967f6aca3ba11b9487
yujmo <yujmo@users.noreply.github.com>
2018-04-27 23:48:55 +0800

    Update start.sh

--- Docker/matlab_mdce/start.sh
+++ Docker/matlab_mdce/start.sh
@@ -4,11 +4,15 @@
 #port3: 27352
 #port4: 27353
 
-#node_name=`echo $MY_POD_NAME |cut -d . -f 1`
-
+echo '{"service": {"name": "node", "tags": ["$MY_POD_NAME"]}}' | tee /consul/config/node.json 
 
 echo "101.132.149.154 student" >> /etc/hosts 
-echo "nameserver dns.default.svc.cluster.local" > /etc/resolv.conf
-echo '{"service": {"name": "node", "tags": ["$MY_POD_NAME"]}}' | sudo tee /consul/config/node.json 
-/home/mdce/toolbox/distcomp/bin/mdce start &
 consul agent -bind 0.0.0.0 -join dns.default.svc.cluster.local -data-dir /consul/data/ -config-dir /consul/config/ -enable-script-checks=true
+
+dns_ip=`consul members |grep server | awk '{print $2}' | cut -d : -f 1`
+
+echo "nameserver $dns_ip" > /etc/resolv.conf
+
+/home/mdce/toolbox/distcomp/bin/mdce start 
+
+ping 127.0.0.1 > /dev/null
