e65e669e7b05e65c1404470125a2af886ea5e0a3
yujmo <yujmo@users.noreply.github.com>
2018-04-27 11:06:57 +0800

    Update start.sh

--- Docker/matlab_dns/start.sh
+++ Docker/matlab_dns/start.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+       
+#port1 8300
+#port2 8301
+#port3 8302
+#port4 8500  
+#port5 8600
+#port6 53
+
 dnsmasq -D &
 consul agent -server -bind 0.0.0.0 -data-dir /consul/ata/ -config-dir /consul/config/ -enable-script-checks=true
