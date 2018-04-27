f74b3ef90bf391ede5d372916c0037b18ee62501
yujmo <yujmo@users.noreply.github.com>
2018-04-27 23:52:46 +0800

    Update start.sh

--- Docker/matlab_mjs/start.sh
+++ Docker/matlab_mjs/start.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 echo "101.132.149.154 student" >> /etc/hosts
-echo "nameserver dns.default.svc.cluster.local" > /etc/resolv.conf
-echo '{"service": {"name": "node", "tags": ["mjs"]}}' | sudo tee /consul/config/node.json 
-consul agent -bind 0.0.0.0 -join dns.default.svc.cluster.local -data-dir /consul/data/ -config-dir /consul/config/ -enable-script-checks=true  &
-/home/mjs/toolbox/distcomp/bin/startjobmanager --name xxx -remotehost mjs.node.service.consul -v &
-/home/mjs/toolbox/distcomp/bin/startworker -jobmanagerhost mjs.node.service.consul -jobmanager xxx -remotehost node1.node.service.consul &
 
-/home/mjs/toolbox/distcomp/bin/startworker -jobmanagerhost mjs.node.service.consul -jobmanager xxx -remotehost node2.node.service.consul
+echo '{"service": {"name": "node", "tags": ["mjs"]}}' | tee /consul/config/node.json 
+
+consul agent -bind 0.0.0.0 -join dns.default.svc.cluster.local -data-dir /consul/data/ -config-dir /consul/config/ -enable-script-checks=true  &
+
+/home/mjs/toolbox/distcomp/bin/startjobmanager --name xxx -remotehost mjs.node.service.consul -v 
+/home/mjs/toolbox/distcomp/bin/startworker -jobmanagerhost mjs.node.service.consul -jobmanager xxx -remotehost node1.node.service.consul 
+/home/mjs/toolbox/distcomp/bin/startworker -jobmanagerhost mjs.node.service.consul -jobmanager xxx -remotehost node2.node.service.consul 
+
+
+dns_ip=`consul members |grep server | awk '{print $2}' | cut -d : -f 1`
+
+echo "nameserver $dns_ip" > /etc/resolv.conf
+
+ping 127.0.0.1 > /dev/null
