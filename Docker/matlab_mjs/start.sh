60d4e623c6c7041b76c9f9ef3c91bca038c61609
yujmo <yujmo@users.noreply.github.com>
2018-04-27 20:25:51 +0800

    Update start.sh

--- Docker/matlab_mjs/start.sh
+++ Docker/matlab_mjs/start.sh
@@ -5,4 +5,4 @@ consul agent -bind 0.0.0.0 -join dns.node.service.consul -data-dir /consul/data/
 /home/mjs/toolbox/distcomp/bin/startjobmanager --name xxx -remotehost mjs.node.service.consul -v &
 /home/mjs/toolbox/distcomp/bin/startworker -jobmanagerhost mjs.node.service.consul -jobmanager xxx -remotehost node1.node.service.consul &
 
-/home/mjs/toolbox/distcomp/bin/startworker -jobmanagerhost mjs.node.service.consul -jobmanager xxx -remotehost node2.node.service.consul &
+/home/mjs/toolbox/distcomp/bin/startworker -jobmanagerhost mjs.node.service.consul -jobmanager xxx -remotehost node2.node.service.consul
