b9b02e867fdc88dc2ee713623ba377bdf9e5d282
yujmo <yujmo@users.noreply.github.com>
2018-04-27 19:30:01 +0800

    Update start.sh

--- Docker/matlab_mdce/start.sh
+++ Docker/matlab_mdce/start.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+#port1: 32000               -               
+#port2: 4455
+#port3: 27352
+#port4: 27353
+
+echo "nameserver dns.node.service.consul" > /etc/resolv.conf
+echo '{"service": {"name": "node", "tags": ["node1"]}}' | sudo tee /consul/config/node.json 
+consul agent -bind 0.0.0.0 -join dns.node.service.consul -data-dir /consul/data/ -config-dir /consul/config/ -enable-script-checks=true  &
+/home/mdce/toolbox/distcomp/bin/mdce start &
