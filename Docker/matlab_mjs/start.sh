b5a0f8dd83f14166561c0d923fbb3c89e2487004
yujmo <yujmo@users.noreply.github.com>
2018-04-27 20:56:09 +0800

    Update start.sh

--- Docker/matlab_mjs/start.sh
+++ Docker/matlab_mjs/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "nameserver dns.node.service.consul" > /etc/resolv.conf
+echo "nameserver dns.default.svc.cluster.local" > /etc/resolv.conf
 echo '{"service": {"name": "node", "tags": ["mjs"]}}' | sudo tee /consul/config/node.json 
-consul agent -bind 0.0.0.0 -join dns.node.service.consul -data-dir /consul/data/ -config-dir /consul/config/ -enable-script-checks=true  &
+consul agent -bind 0.0.0.0 -join dns.default.svc.cluster.local -data-dir /consul/data/ -config-dir /consul/config/ -enable-script-checks=true  &
 /home/mjs/toolbox/distcomp/bin/startjobmanager --name xxx -remotehost mjs.node.service.consul -v &
 /home/mjs/toolbox/distcomp/bin/startworker -jobmanagerhost mjs.node.service.consul -jobmanager xxx -remotehost node1.node.service.consul &
 
