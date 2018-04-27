db6311a671bc5628983045ad028736292e0591c9
yujmo <yujmo@users.noreply.github.com>
2018-04-27 20:24:58 +0800

    Update start.sh

--- Docker/matlab_license/start.sh
+++ Docker/matlab_license/start.sh
@@ -4,4 +4,4 @@
 echo "nameserver dns.node.service.consul" > /etc/resolv.conf
 echo '{"service": {"name": "node", "tags": ["license"]}}' | sudo tee /consul/config/node.json 
 consul agent -bind 0.0.0.0 -join dns.node.service.consul -data-dir /consul/data/ -config-dir /consul/config/ -enable-script-checks=true  &
-sudo -u admin /home/matlab/etc/lmstart &
+sudo -u admin /home/matlab/etc/lmstart 
