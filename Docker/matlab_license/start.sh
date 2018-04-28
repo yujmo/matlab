14d455e8d55dd3494f2648e4e3d6515ddf4a0d13
yujmo <yujmo@users.noreply.github.com>
2018-04-28 13:31:04 +0800

    Update start.sh

--- Docker/matlab_license/start.sh
+++ Docker/matlab_license/start.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
 #port1:27000                   -               
 #port2:32847 
-echo "nameserver dns.node.service.consul" > /etc/resolv.conf
-echo '{"service": {"name": "node", "tags": ["license"]}}' | sudo tee /consul/config/node.json 
-consul agent -bind 0.0.0.0 -join dns.default.svc.cluster.local -data-dir /consul/data/ -config-dir /consul/config/ -enable-script-checks=true  &
+apt-get install iputils-ping net-tools -y
+echo {\"service\": {\"name\": \"node\", \"tags\": [\"$MY_POD_NAME\"]}} | tee /consul/config/node.json 
+
+consul agent -bind 0.0.0.0 -join dns -data-dir /consul/data/ -config-dir /consul/config/ -enable-script-checks=true &
+
+STRING=`netstat -ntulp|grep consul`
+
+while [ -z "$STRING" ];do
+    sleep 1
+    STRING=`netstat -ntulp|grep consul`
+done
+
+dns_ip=`consul members |grep server | awk '{print $2}' | cut -d : -f 1`
+echo "nameserver $dns_ip" > /etc/resolv.conf
+
 sudo -u admin /home/matlab/etc/lmstart 
+
+cp /etc/hosts /etc/hosts.bak 
+xx=`consul members |grep client | awk -F : '{print $1}' | awk '{print $2 , $1}'`
+while true
+do
+    yy=`consul members |grep client | awk -F : '{print $1}' | awk '{print $2 , $1}'`
+    if [ "$xx" = "$yy" ];then
+        consul members |grep client | awk -F : '{print $1}' | awk '{print $2 , $1}' > /etc/hosts
+        cat /etc/hosts.bak >> /etc/hosts
+    fi
+    sleep 1
+done
