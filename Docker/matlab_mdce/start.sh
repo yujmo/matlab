0b0012942e8c14f1ede121f222c33b55cfd9dd19
yujmo <yujmo@users.noreply.github.com>
2018-04-28 13:23:41 +0800

    Update start.sh

--- Docker/matlab_mdce/start.sh
+++ Docker/matlab_mdce/start.sh
@@ -7,6 +7,7 @@ apt-get install iputils-ping net-tools -y
 sed -i "s/admin/student/g" /home/mdce/licenses/network.lic 
 echo {\"service\": {\"name\": \"node\", \"tags\": [\"$MY_POD_NAME\"]}} | tee /consul/config/node.json 
 echo "101.132.149.154 student" >> /etc/hosts 
+
 consul agent -bind 0.0.0.0 -join dns -data-dir /consul/data/ -config-dir /consul/config/ -enable-script-checks=true &
 
 STRING=`netstat -ntulp|grep consul`
@@ -20,4 +21,14 @@ dns_ip=`consul members |grep server | awk '{print $2}' | cut -d : -f 1`
 echo "nameserver $dns_ip" > /etc/resolv.conf
 /home/mdce/toolbox/distcomp/bin/mdce start 
 
-ping 127.0.0.1 > /dev/null
+cp /etc/hosts /etc/hosts.bak 
+xx=`consul members |grep client | awk -F : '{print $1}' | awk '{print $2 , $1}'`
+while true
+do
+    yy=consul members |grep client | awk -F : '{print $1}' | awk '{print $2 , $1}'
+    if [ "$xx" = "$yy" ];then
+        consul members |grep client | awk -F : '{print $1}' | awk '{print $2 , $1}' > /etc/hosts
+        cat /etc/hosts.bak >> /etc/hosts
+    fi
+    sleep 1
+done
