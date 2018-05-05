c381119b4c7f12c8c6888b22c6f793562af7e6d0
yujmo <yujmo@users.noreply.github.com>
2018-05-05 14:53:55 +0800

    Update start.sh

--- Docker/matlab_mjs/start.sh
+++ Docker/matlab_mjs/start.sh
@@ -46,8 +46,8 @@ cat /etc/hosts.bak >> /etc/hosts
 
 /home/mjs/toolbox/distcomp/bin/mdce start 
 /home/mjs/toolbox/distcomp/bin/startjobmanager -name xxx -remotehost mjs -v 
-/home/mjs/toolbox/distcomp/bin/startworker -jobmanagerhost mjs -jobmanager xxx -remotehost node1
-/home/mjs/toolbox/distcomp/bin/startworker -jobmanagerhost mjs -jobmanager xxx -remotehost node2
+/home/mjs/toolbox/distcomp/bin/startworker -jobmanagerhost mjs -jobmanager xxx -remotehost node1 -v
+/home/mjs/toolbox/distcomp/bin/startworker -jobmanagerhost mjs -jobmanager xxx -remotehost node2 -v
 
 mkdir -p /var/run/sshd
 
